179a97df202e4d175eec14758bd01785dcf3734f
DaniFoldi <dani@stefan.hu>
2022-04-15 16:05:36 +0000

    :vertical_traffic_light: Add rule manipulation tests

---
+++ __tests__/1.ruleset-manipulation.ts
@@ -0,0 +1,53 @@
+import { Ruleset } from '../src/types'
+import { addRuleset, deleteAllRules, deleteRuleset, getRuleset } from '../src/index'
+
+const ruleset1: Ruleset = {
+
+}
+
+beforeEach(() => {
+  deleteAllRules()
+})
+
+test('adds a rule to list', () => {
+  expect(() => getRuleset('rule')).toThrow()
+  addRuleset('rule', ruleset1)
+  expect(getRuleset('rule')).toEqual(ruleset1)
+})
+
+test('fails to add existing rule', () => {
+  expect(() => getRuleset('rule')).toThrow()
+  addRuleset('rule', ruleset1)
+  expect(() => addRuleset('rule', ruleset1)).toThrow()
+})
+
+test('gets rule by id', () => {
+  expect(() => getRuleset('rule')).toThrow()
+  addRuleset('rule', ruleset1)
+  expect(getRuleset('rule')).toEqual(ruleset1)
+})
+
+test('fails to get nonexistent rule', () => {
+  expect(() => getRuleset('rule')).toThrow()
+})
+
+test('deletes a rule from list', () => {
+  expect(() => getRuleset('rule')).toThrow()
+  addRuleset('rule', ruleset1)
+  expect(getRuleset('rule')).toEqual(ruleset1)
+  deleteRuleset('rule')
+  expect(() => getRuleset('rule')).toThrow()
+})
+
+test('fails to delete nonexistent rule', () => {
+  expect(() => getRuleset('rule')).toThrow()
+  expect(() => deleteRuleset('rule')).toThrow()
+})
+
+test('deletes all rules from list', () => {
+  expect(() => getRuleset('rule')).toThrow()
+  addRuleset('rule', ruleset1)
+  expect(getRuleset('rule')).toEqual(ruleset1)
+  deleteAllRules()
+  expect(() => getRuleset('rule')).toThrow()
+})
