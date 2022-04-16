5fdda06c2b4a2bd715df121a9dc535a219adae9d
DaniFoldi <dani@stefan.hu>
2022-04-16 13:09:56 +0200

    :zap: Simplify type definitions

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,17 @@
 export type Ruleset = RuleId | {anyOf: Ruleset[]} | {allOf: Ruleset[]} | {noneOf: Ruleset[]}
 
-export type Result = 'allow' | 'deny' | (() => ('allow' | 'deny')) | Promise<void>
+export type Result = 'allow' | 'deny'
 
 export interface RuleContext {}
+
+export type RuleOptions<T> = {
+  [key in keyof T]: T[key]
+}
 
 export type RuleId = string
 
 export type RulesetId = string | number
 
 export type RulesetStore = Record<RulesetId, Ruleset>
+
+export type Rule<T> = (context: RuleContext, options: RuleOptions<T>) => Result
