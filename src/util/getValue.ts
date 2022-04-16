3fcf83f45d7b930e88337490080a6d592d9f5365
DaniFoldi <dani@stefan.hu>
2022-04-16 15:06:25 +0200

    :hammer: Fix getValue

--- src/util/getValue.ts
+++ src/util/getValue.ts
@@ -1,6 +1,6 @@
 import { RuleContext } from "../types";
 
-export default function(context: RuleContext, key: string): any {
+export default function(context: RuleContext, key?: string): any {
   if (!key) {
     return context
   }
@@ -10,7 +10,7 @@ export default function(context: RuleContext, key: string): any {
 
   for (let i = 0; i < keys.length; i++) {
     if (!value[keys[i]]) {
-      return value
+      throw new Error(`Key ${key} does not exist in context`)
     }
     value = value[keys[i]]
   }
