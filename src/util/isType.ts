d33c411feb89a3e9ef69d93621b9674b8d1495a4
DaniFoldi <dani@stefan.hu>
2022-04-16 18:09:55 +0200

    :zap: Add rule type check functions

---
+++ src/util/isType.ts
@@ -0,0 +1,17 @@
+import { Ruleset, Rule } from "../types"
+
+export function isRule(ruleset: Ruleset): ruleset is Rule {
+  return Array.isArray(ruleset) && ruleset.length === 2
+}
+
+export function isAnyOf(ruleset: Ruleset): ruleset is {anyOf: Ruleset[]} {
+  return Object.hasOwn(ruleset, 'anyOf')
+}
+
+export function isAllOf(ruleset: Ruleset): ruleset is {allOf: Ruleset[]} {
+  return Object.hasOwn(ruleset, 'allOf')
+}
+
+export function isNoneOf(ruleset: Ruleset): ruleset is {noneOf: Ruleset[]} {
+  return Object.hasOwn(ruleset, 'noneOf')
+}
